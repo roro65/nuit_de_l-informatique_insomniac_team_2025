8cd1bc410ea985f42950b739b539a2381c0cb6b1
Samuel-Duhem <samuel.duhem@etu.iut-tlse3.fr>
2025-12-05 00:02:26 +0100

    test des pages

--- header_footer.php
+++ header_footer.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact - Informatique - Toulouse</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/header_style.css">
 </head>
 <header>
     <nav>
--- index.php
+++ index.php
@@ -5,25 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/globals.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/header_style.css">
     <title>Document</title>
 </head>
 <header>
+    <nav>
+        
+        <input type="checkbox" id="burger-toggle">
+        <label for="burger-toggle" class="burger-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+        <ul class="menu list-item">
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+        </ul>
 
+        <div class="bar-recherche">
+        <input type="text" placeholder="Entrez votre recherche">
+        <button>Search</button>
+        
+        </div>
+
+        <img class="logo-NIRD" src="NIRD" alt="logo NIRD">
+    </nav>
 </header>
 <body>
     <div>
         <h1>Hello, World!</h1>
     </div>
     <div>
-        <h2></h2>
-        <p></p>
+        <h2>Titre</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
     <div>
-        <h2></h2>
-        <p></p>
+        <h2>Titre</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>
     </div>
     <div id="box-holder">
-        <h2></h2>
+        <h2>Quizz</h2>
         <div class="box-D">box 1</div>
         <div class="box-G">box 2</div>
         <div class="box-D">box 3</div>
