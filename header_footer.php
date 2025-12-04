8a83ddd6e2cb68564116973a2d874e3bf9dc38fe
magispeur <mathis.briere@etu.iut-tlse3.fr>
2025-12-04 23:23:38 +0100

    header sans footer

--- header_footer.php
+++ header_footer.php
@@ -1,12 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contact - Informatique - Toulouse</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-   
-    
-</body>
+<header>
+    <nav>
+        <input type="checkbox" id="burger-toggle">
+
+        <label for="burger-toggle" class="burger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+
+        <ul class="menu list-item">
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+        </ul>
+
+        <div class="bar-recherche">
+        <input type="text" placeholder="Entrez votre recherche">
+        <button>Search</button>
+        </div>
+
+        <img class="logo-NIRD" src="NIRD" alt="logo NIRD">
+    </nav>
+</header>
+
 </html>
