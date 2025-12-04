5c82af4dcba56fad5871b0236b7046cd14bd9efb
ANPatrice <antoinepatrice973@gmail.com>
2025-12-05 00:23:32 +0100

    Create Informations.php

---
+++ Informations.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/globals.css">
+    <title>NIRD - A propos</title>
+</head>
+<header>
+</header>
+
+
+<body>
+    
+
+    <div>
+        <h1>Qui Sommes nous ?</h1>
+        Ce projet est réalisé par des étudiants de BUT Informatique dans le cadre du défi national de la nuit de l'info édition 2025
+        Ce site a pour objectif de proposer des solutions contre la dépendance du numérique, ce site a pour but de présenter cette démarche soutenue par le collectif étudiant NIRD
+        Ce projet étant un projet étudiant et non officielle est suceptible de contenir des erreurs il est donc conseiller de vérifier son contenue sur d'autres sites
+
+
+    </div>
+    
+    <div>
+        <h1>Composition de l'Équipe</h1>
+        <div>
+            <p>
+                PLEINDOUX Lysianne <br>
+                ANTOINE Patrice <br>
+                GIBRAT Grégoire <br>
+                GAY Louis <br>
+                BARRERE Valentin <br>
+                DUHEM Samuel <br>
+                COSINUS-VERGUDIER Adrien <br>
+                BRIERE Mathis <br>
+                Maxime <br>
+                Vidian <br>
+                BENDRELL Nathan <br>      
+            </p>
+            <img src=".jpg" alt="Image de l'équipe" >
+        </div>
+        
+    </div>
+
+    <div>
+        <h1>
+            Contactez-Nous
+        </h1>
+        antoinepatrice973@gmail.com
+    </div>
+
+
+
+</body>
+
