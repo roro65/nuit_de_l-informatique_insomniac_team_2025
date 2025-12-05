913e9390e67fb6b998327035c266eeec8855f7fe
BetaTetras <gibrat.gregoire@gmail.com>
2025-12-05 05:06:05 +0100

    Test js 4

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const btnSearch = document.getElementById("Search");
-const Searching = document.getElementById("SearchBar");
-
-btnSearch.onclick = function(){
-    if(Searching.value == "snake"){
-        window.open('Snake/Snake/index.html', '_top');
-    }
-}
-
 function ActionListener(Game){
     const SequenceA = ["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight","A","B"];
     const SequenceB = ["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight","a","b"];
-    let index;
+    
+    let index = 0;
+
     document.addEventListener("keydown",(event) =>{
         const key = event.key;
+
+        // si la touche correspond
         if (key === SequenceA[index] || key === SequenceB[index]) {
             index++;
+            console.log("OK:", key);
+
             if (index === SequenceA.length) {
                 window.open('Snake/SnakeHard/index.html', '_top');
                 index = 0;
@@ -22,6 +19,5 @@ function ActionListener(Game){
         } else {
             index = 0;
         }
-
     });
 }
