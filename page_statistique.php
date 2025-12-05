3016c06df50b5ab37fc96232db91b7d207e3b308
Valentin Barrere <valentin.barrere@etu.iut-tlse3.fr>
2025-12-05 01:06:00 +0100

    Create page_statistique.php

---
+++ page_statistique.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Statistique</title>
+		<link rel="stylesheet" href="./css/globals.css">
+		<link rel="stylesheet" href="./css/header_style.css">
+		
+	</head>
+	<header>
+    <nav>
+        <input type="checkbox" id="burger-toggle">
+
+        <label for="burger-toggle" class="burger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+
+        <ul class="menu list-item">
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+                <li><a href="#">jdzefhzfiz</a></li>
+        </ul>
+
+        <div class="bar-recherche">
+        <input type="text" placeholder="Entrez votre recherche">
+        <button>Search</button>
+        </div>
+
+        <img class="logo-NIRD" src="NIRD" alt="logo NIRD">
+    </nav>
+</header>
+	<body>
+		<h1>Page statistique</h1>
+
+		<div>
+			<h2>Statistiques des visiteurs avant la découverte du site</h2>
+			<p>Voici quelques statistiques sur les connaissances de l'ensemble des visiteurs avant de découvrir les informations du site.</p>
+			<div id="stat_avant">
+
+			</div>
+		</div>
+
+		<div>
+			<h2>Évolution personnelle des connaissances</h2>
+			<p>Voici l'évolution de vos résultats après vous être renseigné sur notre site.</p>
+			<div id="stat_apres">
+
+			</div>
+		</div>
+	</body>
+</html>
