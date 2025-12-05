9ff7d9dd64c8485f000d30f8a99766e81bf3b7e5
Valentin Barrere <valentin.barrere@etu.iut-tlse3.fr>
2025-12-05 02:04:12 +0100

    Update page_statistique.php

--- page_statistique.php
+++ page_statistique.php
@@ -5,51 +5,61 @@
 		<title>Statistique</title>
 		<link rel="stylesheet" href="./css/globals.css">
 		<link rel="stylesheet" href="./css/header_style.css">
+		   
 		
 	</head>
-	<header>
-    <nav>
-        <input type="checkbox" id="burger-toggle">
-
-        <label for="burger-toggle" class="burger-icon">
-            <span></span>
-            <span></span>
-            <span></span>
-        </label>
-
-        <ul class="menu list-item">
-                <li><a href="#">jdzefhzfiz</a></li>
-                <li><a href="#">jdzefhzfiz</a></li>
-                <li><a href="#">jdzefhzfiz</a></li>
-                <li><a href="#">jdzefhzfiz</a></li>
-                <li><a href="#">jdzefhzfiz</a></li>
-        </ul>
-
-        <div class="bar-recherche">
-        <input type="text" placeholder="Entrez votre recherche">
-        <button>Search</button>
-        </div>
-
-        <img class="logo-NIRD" src="NIRD" alt="logo NIRD">
-    </nav>
-</header>
+	
 	<body>
-		<h1>Page statistique</h1>
+		<header>
+			<nav>
+				<input type="checkbox" id="burger-toggle">
 
-		<div>
-			<h2>Statistiques des visiteurs avant la découverte du site</h2>
-			<p>Voici quelques statistiques sur les connaissances de l'ensemble des visiteurs avant de découvrir les informations du site.</p>
-			<div id="stat_avant">
+				<label for="burger-toggle" class="burger-icon">
+					<span></span>
+					<span></span>
+					<span></span>
+				</label>
 
-			</div>
-		</div>
+				<ul class="menu list-item">
+						<li><a href="#">jdzefhzfiz</a></li>
+						<li><a href="#">jdzefhzfiz</a></li>
+						<li><a href="#">jdzefhzfiz</a></li>
+						<li><a href="#">jdzefhzfiz</a></li>
+						<li><a href="#">jdzefhzfiz</a></li>
+				</ul>
 
-		<div>
-			<h2>Évolution personnelle des connaissances</h2>
-			<p>Voici l'évolution de vos résultats après consultation du site.</p>
-			<div id="stat_apres">
+				<div class="bar-recherche">
+				<input type="text" placeholder="Entrez votre recherche">
+				<button>Search</button>
+				</div>
 
-			</div>
+				<img class="logo-NIRD" src="NIRD" alt="logo NIRD">
+			</nav>
+		</header>
+
+		<div class="container">
+			<main>
+				<h2>Page statistique</h2>
+				<br>
+
+				<div class="nird-section">
+					<h2>Statistiques des visiteurs avant la découverte du site</h2>
+					<p>Voici quelques statistiques sur les connaissances de l'ensemble des visiteurs avant de découvrir les informations du site.</p>
+					<div id="stat_avant">
+
+					</div>
+				</div>
+				<br>
+
+				<div class="nird-section">
+					<h2>Évolution personnelle des connaissances</h2>
+					<p>Voici l'évolution de vos résultats après consultation du site.</p>
+					<div id="stat_apres">
+
+					</div>
+				</div>
+				<br>
+			</main>
 		</div>
 	</body>
 </html>
