49f9fe10c171ab8dbe4841e884bf2eec7bde8fe0
BetaTetras <gibrat.gregoire@gmail.com>
2025-12-05 04:11:05 +0100

    Test js 3

--- text.js
+++ text.js
@@ -3,6 +3,6 @@ const Searching = document.getElementById("SearchBar");
 
 btnSearch.onclick = function(){
     if(Searching.value == "snake"){
-        window.open('Snake/index.html', '_top');
+        window.open('Snake/Snake/index.html', '_top');
     }
 }
