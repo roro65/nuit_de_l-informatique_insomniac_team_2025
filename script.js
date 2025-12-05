cfed4112bffe87123ce0cf0f98604b7c3abaad1a
BetaTetras <gibrat.gregoire@gmail.com>
2025-12-05 04:58:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ btnSearch.onclick = function(){
         window.open('Snake/Snake/index.html', '_top');
     }
 }
+
+function ActionListener(Game){
+    const SequenceA = ["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight","A","B"];
+    const SequenceB = ["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight","a","b"];
+    let index;
+    document.addEventListener("keydown",(event) =>{
+        const key = event.key;
+        if (key === SequenceA[index] || key === SequenceB[index]) {
+            index++;
+            if (index === SequenceA.length) {
+                window.open('Snake/SnakeHard/index.html', '_top');
+                index = 0;
+            }
+        } else {
+            index = 0;
+        }
+
+    });
+}
